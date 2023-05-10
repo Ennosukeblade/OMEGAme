115893faabecadc5077a668953e12898c79c4c74
oussama0710 <ouxavi80@gmail.com>
2023-05-10 16:09:15 +0100

    Migrations

---
+++ server/Migrations/20230508132241_FifthMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class FifthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isPlayable",
+                table: "Games",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isPlayable",
+                table: "Games");
+        }
+    }
+}
